2124e303a976cb8936bfdfc3c267af0523ef6e4e
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2023-09-28 00:27:57 +0530

    working snake

--- snake_game/helper.py
+++ snake_game/helper.py
@@ -13,6 +13,14 @@ class Orientation(Enum):
     DOWN = [0, 1]
     LEFT = [-1, 0]
 
+class TileValue(Enum):
+    EMPTY = 0.0
+    SNAKE = 1.1
+    SNAKE_HEAD = 1.2
+    WALL = -1.0
+    FOOD = 2.0
+
+
 def calculate_next_direction(orientation, direction):
     direction_mapping = {
         (Orientation.UP, MoveDirection.LEFT): Orientation.LEFT,
@@ -32,45 +40,26 @@ class Snake:
         self.length = length
         self.breadth = breadth
         self.snake_size = snake_size
-        field = np.zeros((length, breadth))
-        field = np.insert(field, 0, -1, axis=0)
-        field = np.insert(field, length+1, -1, axis=0)
-        field = np.insert(field, 0, -1, axis=1)
-        field = np.insert(field, breadth+1, -1, axis=1)
+        field = np.full((length, breadth), TileValue.EMPTY.value)
+        field = np.insert(field, 0, TileValue.WALL.value, axis=0)
+        field = np.insert(field, length+1, TileValue.WALL.value, axis=0)
+        field = np.insert(field, 0, TileValue.WALL.value, axis=1)
+        field = np.insert(field, breadth+1, TileValue.WALL.value, axis=1)
         self.field = field
 
     def start_position(self):
         x, y = 6, 3
         head_position = np.array([x, y, Orientation.RIGHT, MoveDirection.FORWARD])
         tail_position = np.array([x-self.snake_size+1, y, Orientation.RIGHT, MoveDirection.FORWARD])
-        self.field[y][x] = 1
-        self.field[y][x-self.snake_size+1] = 1
+        self.field[y][x] = TileValue.SNAKE.value
+        self.field[y][x-self.snake_size+1] = TileValue.SNAKE.value
         for i in range(1, self.snake_size):
-            self.field[y][x-i] = 1
+            self.field[y][x-i] = TileValue.SNAKE.value
         self.snake = np.array([head_position, tail_position])
 
     def move(self, parent_move_dirr=MoveDirection.FORWARD):
+        print(parent_move_dirr)
         # move the snake
-
-        # add new head
-        first_block = np.copy(self.snake[0])
-        dirr = first_block[2]
-        # move_dirr = first_block[3]
-        move_dirr = parent_move_dirr
-        direction = calculate_next_direction(dirr, move_dirr)
-        direction_value = direction.value
-        new_head = np.array([first_block[0] + direction_value[0], first_block[1] + direction_value[1], direction, move_dirr])
-        # check if new head is in snake body or wall
-        if self.field[new_head[1]][new_head[0]] in [-1, 1]:
-            self.field = np.zeros((self.length, self.breadth))
-            return False
-        if move_dirr == MoveDirection.FORWARD:
-            self.snake[0] = new_head
-        else:
-            self.snake = np.insert(self.snake, 0, new_head, axis=0)
-        self.field[new_head[1]][new_head[0]] = 1
-
-
         # remove last snake block
         # if snake[-1] - snake[-2] == 1:
         #     remove snake[-1]
@@ -85,6 +74,7 @@ class Snake:
         diff = abs(x_diff) + abs(y_diff)
         if diff == 1:
             self.snake = np.delete(self.snake, -1, axis=0)
+
         else:
             dirr = last_block[2]
             move_dirr = last_block[3]
@@ -95,7 +85,28 @@ class Snake:
             self.snake[-1][0] = new_last_block[0]
             self.snake[-1][1] = new_last_block[1]
         # make last block 0
-        self.field[last_block[1]][last_block[0]] = 0
+        self.field[last_block[1]][last_block[0]] = TileValue.EMPTY.value
+
+
+
+        # add new head
+        first_block = np.copy(self.snake[0])
+        self.field[first_block[1]][first_block[0]] = TileValue.SNAKE.value
+        dirr = first_block[2]
+        # move_dirr = first_block[3]
+        move_dirr = parent_move_dirr
+        direction = calculate_next_direction(dirr, move_dirr)
+        direction_value = direction.value
+        new_head = np.array([first_block[0] + direction_value[0], first_block[1] + direction_value[1], direction, move_dirr])
+        # check if new head is in snake body or wall
+        if self.field[new_head[1]][new_head[0]] in [TileValue.SNAKE.value, TileValue.WALL.value, TileValue.SNAKE_HEAD.value]:
+            self.field = np.zeros((self.length, self.breadth))
+            return False
+        if move_dirr == MoveDirection.FORWARD and len(self.snake) > 1:
+            self.snake[0] = new_head
+        else:
+            self.snake = np.insert(self.snake, 0, new_head, axis=0)
+        self.field[new_head[1]][new_head[0]] = TileValue.SNAKE_HEAD.value
         
         return True
 
--- snake_game/main.py
+++ snake_game/main.py
@@ -2,29 +2,42 @@ import time
 from helper import *
 
 # init
-init_length = 5
+init_length = 2
 frame_width = 10
 frame_height = 10
 
 snake = Snake(frame_width, frame_height, init_length)
 snake.start_position()
+# # snake.move(MoveDirection.FORWARD)
+# snake.move(MoveDirection.RIGHT)
+# # snake.move(MoveDirection.FORWARD)
+# print(snake.field)
+# print(snake.snake)
+# snake.move(MoveDirection.RIGHT)
+# print(snake.field)
+# print(snake.snake)
 # snake.move(MoveDirection.FORWARD)
 # print(snake.field)
+# print(snake.snake)
 # snake.move(MoveDirection.RIGHT)
 # print(snake.field)
+# print(snake.snake)
 # snake.move(MoveDirection.RIGHT)
 # print(snake.field)
-# snake.move(MoveDirection.RIGHT)
+# print(snake.snake)
+# snake.move(MoveDirection.FORWARD)
 # print(snake.field)
+# print(snake.snake)
+
 
 # game loop
 temp = True
 while temp:
     direction = np.random.choice([MoveDirection.LEFT, MoveDirection.RIGHT, MoveDirection.FORWARD])
-    print(direction)
     temp = snake.move(direction)
+    print(snake.snake)
     print(snake.field)
-    time.sleep(1)
+    # time.sleep(1)
     print("-------------------------------------------------")
 
 
